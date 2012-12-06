0005b0a110e4ab0c97e9ce8d60150f8cd3f0c8e9
Daniel Somers <d.somers@ulcc.ac.uk>
2012-12-06 15:37:15 +0000

    testing

--- settings.php
+++ settings.php
@@ -3,19 +3,19 @@
 require_once(dirname(__FILE__) . '/../../config.php');
 require_login(0, false);
 
-global $CFG;
+global $CFG, $OUTPUT;
 
 //$PAGE->set_url('/local/ualfrontpage/');
-$context = get_context_instance(CONTEXT_SYSTEM);
-$PAGE->set_context($context);
-$PAGE->set_title(get_string('pluginname', 'local_ualfrontpage'));
+//$context = get_context_instance(CONTEXT_SYSTEM);
+//$PAGE->set_context($context);
+//$PAGE->set_title(get_string('pluginname', 'local_ualfrontpage'));
 
-$PAGE->navbar->add(get_string('pluginname', 'local_ualfrontpage'));
+//$PAGE->navbar->add(get_string('pluginname', 'local_ualfrontpage'));
 
 $content='';
 
-$content .= $OUTPUT->header();
-$content .= $OUTPUT->heading(get_string('pluginname', 'local_ualfrontpage'));
+//$content .= $OUTPUT->header();
+//$content .= $OUTPUT->heading(get_string('pluginname', 'local_ualfrontpage'));
 
 $hassiteconfig = has_capability('moodle/site:config', $context);
 
@@ -23,6 +23,6 @@ if($hassiteconfig) {
     $content .= ' This will be where you can configure the front page content :)';
 }
 
-$content .= $OUTPUT->footer();
+//$content .= $OUTPUT->footer();
 
 echo $content;
