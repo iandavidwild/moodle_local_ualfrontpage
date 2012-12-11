3c486a9516a49d9e24e920770ff34c946954b993
Daniel Somers <d.somers@ulcc.ac.uk>
2012-12-11 10:40:47 +0000

    changed image url

--- index.php
+++ index.php
@@ -37,9 +37,9 @@ if($hassiteconfig) {
     //
     
     #$dir = $CFG->dataroot .'/filedir/';
-    $dir = $CFG->dataroot .'../../docroot/ualmoodle/theme/ual/pix/';
+    $dir = $CFG->dataroot .'/theme/ual/pix/';
     #$image_dir = $CFG->dataroot .'/filedir/';
-    $image_dir = '../../docroot/ualmoodle/theme/ual/pix/';
+    $image_dir = '/theme/ual/pix/';
     $existing_files = '';
     $number_of_images = $DB->count_records('image_rotator', array('status'=>'1'));
     
