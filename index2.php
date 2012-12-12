2669dc9836b19f8fda9f0fb894939636bd43ec0e
Daniel Somers <d.somers@ulcc.ac.uk>
2012-12-12 14:10:13 +0000

    added section edit

--- index2.php
+++ index2.php
@@ -1,159 +1,91 @@
 <?php
 
 require_once(dirname(__FILE__) . '/../../config.php');
+//require_once($CFG->dirroot.'/local/ualfrontpage/sectionform.php');
 
 require_login(0, false);
 
-global $CFG, $DB;
+//global $CFG, $DB;
 
 $PAGE->set_url('/local/ualfrontpage/');
+$PAGE->set_pagelayout('standard');
 
-$jslink1 = new moodle_url($CFG->wwwroot.'/lib/editor/tinymce/tiny_mce/3.4.6/tiny_mce.js');
-$jslink2 = new moodle_url($CFG->wwwroot.'/local/ualfrontpage/script/myeditor.js');
-$csslink1 = new moodle_url($CFG->wwwroot.'/lib/editor/tinymce/tiny_mce/3.4.6/themes/advanced/skins/default/ui.css');
-$csslink2 = new moodle_url($CFG->wwwroot.'/lib/editor/tinymce/tiny_mce/3.4.6/plugins/inlinepopups/skins/clearlooks2/window.css');
-
-$PAGE->requires->js($jslink1,true);
-$PAGE->requires->css($jslink2, true);
-$PAGE->requires->css($csslink1, true);
-$PAGE->requires->css($csslink2, true);
-
-//$PAGE->requires->js('/local/ualfrontpage/tinymce/jscripts/tiny_mce/tiny_mce.js',true);
-//$PAGE->requires->js('/local/ualfrontpage/tinymce/jscripts/tiny_mce/myeditor.js',true);
-//$PAGE->requires->css('/local/ualfrontpage/tinymce/jscripts/tiny_mce/themes/advanced/skins/default/ui.css', true);
-//$PAGE->requires->css('/local/ualfrontpage/tinymce/jscripts/tiny_mce/plugins/inlinepopups/skins/clearlooks2/window.css', true);
-
-/*
-$PAGE->requires->js('/tinymce/jscripts/tiny_mce/tiny_mce.js',true);
-$PAGE->requires->js('/tinymce/jscripts/tiny_mce/myeditor.js',true);
-
-$PAGE->requires->css('/tinymce/jscripts/tiny_mce/themes/advanced/skins/default/ui.css', true);
-$PAGE->requires->css('/tinymce/jscripts/tiny_mce/plugins/inlinepopups/skins/clearlooks2/window.css', true);
-*/
 $context = get_context_instance(CONTEXT_SYSTEM);
 $PAGE->set_context($context);
 $PAGE->set_title(get_string('pluginname', 'local_ualfrontpage'));
 
 $PAGE->navbar->add(get_string('pluginname', 'local_ualfrontpage'));
 
-
-$content='';
-
-
-/*tinyMCE.init({
-    mode : "textareas",
-    theme : "advanced",
-    plugins : "autolink,lists,pagebreak,style,layer,table,save,advhr,advimage,advlink,emotions,iespell,inlinepopups,insertdatetime,preview,media,searchreplace,print,contextmenu,paste,directionality,fullscreen,noneditable,visualchars,nonbreaking,xhtmlxtras,template,wordcount,advlist,autosave,visualblocks",
-
-    //// Theme options
-    theme_advanced_buttons1 : "save,newdocument,|,bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,styleselect,formatselect,fontselect,fontsizeselect",
-    theme_advanced_buttons2 : "cut,copy,paste,pastetext,pasteword,|,search,replace,|,bullist,numlist,|,outdent,indent,blockquote,|,undo,redo,|,link,unlink,anchor,image,cleanup,help,code,|,insertdate,inserttime,preview,|,forecolor,backcolor",
-    theme_advanced_buttons3 : "tablecontrols,|,hr,removeformat,visualaid,|,sub,sup,|,charmap,emotions,iespell,media,advhr,|,print,|,ltr,rtl,|,fullscreen",
-    theme_advanced_buttons4 : "insertlayer,moveforward,movebackward,absolute,|,styleprops,|,cite,abbr,acronym,del,ins,attribs,|,visualchars,nonbreaking,template,pagebreak,restoredraft,visualblocks",
-    theme_advanced_toolbar_location : "top",
-    theme_advanced_toolbar_align : "left",
-    theme_advanced_statusbar_location : "bottom",
-    theme_advanced_resizing : true,
-
-    //// Example content CSS (should be your site CSS)
-    //content_css : "css/content.css",
-
-    //// Drop lists for link/image/media/template dialogs
-    template_external_list_url : "lists/template_list.js",
-    external_link_list_url : "lists/link_list.js",
-    external_image_list_url : "lists/image_list.js",
-    media_external_list_url : "lists/media_list.js",
-    
-    });
-*/
-
-$content .= $OUTPUT->header();
-$content .= $OUTPUT->heading(get_string('pluginname', 'local_ualfrontpage'));
+echo $OUTPUT->header();
+echo $OUTPUT->heading(get_string('pluginname', 'local_ualfrontpage'));
 
 $hassiteconfig = has_capability('moodle/site:config', $context);
 
-if($hassiteconfig) {
-    $content .= html_writer::start_tag('div');
-        $content .= html_writer::start_tag('form', array('action'=>'index2.php','method'=>'post', 'name'=>'frmfrontpage', 'enctype'=>'multipart/form-data'));
-            $content .= html_writer::start_tag('table', array('width'=>'100%'));
-                
-                $content .= html_writer::start_tag('tr'); //this row for selecting image
-                    
-                    $content .= html_writer::start_tag('td');
-                        $content .= get_string('section1','local_ualfrontpage');
-                    $content .= html_writer::end_tag('td');
-                    
-                    $content .= html_writer::start_tag('td');
-                        $content .= html_writer::tag('textarea', '', array('name'=>'section1', 'cols'=>'80', 'rows'=>'15'));
-                    $content .= html_writer::end_tag('td');
-                    
-                $content .= html_writer::end_tag('tr');//finish row for selecting image
-
-                $content .= html_writer::start_tag('tr'); //this row for submit botton
-                    
-                    $content .= html_writer::start_tag('td');
-                        $content .= get_string('section5','local_ualfrontpage');
-                    $content .= html_writer::end_tag('td');
-                    
-                    $content .= html_writer::start_tag('td');
-                        $content .= html_writer::tag('textarea', '', array('name'=>'section5', 'cols'=>'80', 'rows'=>'15'));
-                    $content .= html_writer::end_tag('td');
-                    
-                $content .= html_writer::end_tag('tr');
-                
-                $content .= html_writer::start_tag('tr'); //this row for submit botton
-                    
-                    $content .= html_writer::start_tag('td');
-                        $content .= get_string('section3','local_ualfrontpage');
-                    $content .= html_writer::end_tag('td');
-                    
-                    $content .= html_writer::start_tag('td');
-                        $content .= html_writer::tag('textarea', '', array('name'=>'section3', 'cols'=>'80', 'rows'=>'15'));
-                    $content .= html_writer::end_tag('td');
-                    
-                $content .= html_writer::end_tag('tr'); 
-                
-                $content .= html_writer::start_tag('tr'); //this row for submit botton
-                    
-                    $content .= html_writer::start_tag('td');
-                        $content .= get_string('section4','local_ualfrontpage');
-                    $content .= html_writer::end_tag('td');
-                    
-                    $content .= html_writer::start_tag('td');
-                        $content .= html_writer::tag('textarea', '', array('name'=>'section4', 'cols'=>'80', 'rows'=>'15'));
-                        //$content .= html_writer::end_tag('textarea');
-                    $content .= html_writer::end_tag('td');
-                    
-                $content .= html_writer::end_tag('tr'); 
-                
-                $content .= html_writer::start_tag('tr'); //this row for submit botton
-                    
-                    $content .= html_writer::start_tag('td');
-                        //$content .= get_string('section3','local_ualfrontpage');
-                    $content .= html_writer::end_tag('td');
-                    
-                    $content .= html_writer::start_tag('td');
-                        $content .= html_writer::empty_tag('input',array('type'=>'submit', 'name'=>'Save', 'id'=>'Save', 'value'=>'Save'));
-                    $content .= html_writer::end_tag('td');
-                    
-                $content .= html_writer::end_tag('tr'); //finish row for submit button
-                
-            $content .= html_writer::end_tag('table');
-        $content .= html_writer::end_tag('form');
-    $content .= html_writer::end_tag('div');
+class frontpage_section_form extends moodleform {
+    
+    //Add elements to form
+    function definition() {
+        global $CFG, $USER, $OUTPUT, $DB;
+        $mform =& $this->_form; // Don't forget the underscore!
+        
+        $records = $DB->get_records('section');// oder by display_order');
+        $mform->addElement('header', 'general', get_string('sections', 'local_ualfrontpage'));
+        
+        foreach ($records as $record){ 
+            $mform->addElement('editor', 'section1', get_string('section1', 'local_ualfrontpage'));
+            $mform->setDefault('section1', array('text'=>$record->section1, 'format'=>FORMAT_HTML));
+            $mform->addElement('html', '<hr>');
+            $mform->addElement('editor', 'section2', get_string('section2', 'local_ualfrontpage'));
+            $mform->setDefault('section2', array('text'=>$record->section2, 'format'=>FORMAT_HTML));
+            $mform->addElement('html', '<hr>');
+            $mform->addElement('editor', 'section3', get_string('section3', 'local_ualfrontpage'));
+            $mform->setDefault('section3', array('text'=>$record->section3, 'format'=>FORMAT_HTML));
+            $mform->addElement('html', '<hr>');
+            $mform->addElement('editor', 'section4', get_string('section4', 'local_ualfrontpage'));
+            $mform->setDefault('section4', array('text'=>$record->section4, 'format'=>FORMAT_HTML));
+        }    
+        $mform->addElement('submit', 'Save', get_string('save', 'local_ualfrontpage'));
+        //$mform->createElement('submit', 'Save', get_string('save', 'local_ualfrontpage'));
+        $mform->closeHeaderBefore('Save');
+    }
+    //Custom validation should be added here
+    function validation($data, $files) {
+        return array();
+    }
 }
 
-$content .= $OUTPUT->footer();
+if($hassiteconfig) {
+    
+    //all code removed
+   
+}
 
-echo $content;
+$section_form = new frontpage_section_form();
+$section_form->display();
+echo $OUTPUT->footer();
 
-if(isset($_POST['Save'])){
-    $row = new stdClass();
-    $row->section1    = $_POST['section1'];
-    $row->section2    = $_POST['section5'];//sectction 5 is replacing section 2 for unknown reason
-    $row->section3    = $_POST['section3'];
-    $row->section4    = $_POST['section4'];
-    $table = 'section'; //destination table name without prefix, where data will save
-    $DB->insert_record($table, $row);
+//echo $content;
+
+
+if (isset($_POST['Save'])){
+    $section_form_data = $section_form->get_data();
+    $mydata = new stdClass();
+    $mydata->section1 = $section_form_data->section1['text'];
+    $mydata->section2 = $section_form_data->section2['text'];
+    $mydata->section3 = $section_form_data->section3['text'];
+    $mydata->section4 = $section_form_data->section4['text'];
+    
+    $ex_data = $DB->count_records('section');
+    if ($ex_data < 1){
+        //save new data
+        $DB->insert_record('section', $mydata);
+    }else{
+        //save updated data
+        $records = $DB->get_records('section');
+        foreach ($records as $record){
+            $mydata->id = $record->id;
+        }
+        $DB->update_record('section', $mydata);
+    }
 }
 
--- lang/en/local_ualfrontpage.php
+++ lang/en/local_ualfrontpage.php
@@ -3,11 +3,28 @@
 $string['pluginname'] = 'UAL Front Page Settings';
 $string['newsandannouncements'] = 'News and announcements settings';
 $string['maxnewsitems'] = 'Maximum number of items to show';
+$string['fieldname']='Section';
+
+$string['uploadfiles']='Upload Your Files';
+$string['addimage']='Add New Image';
+$string['headertext']='Image Uploader';
+$string['uploadfilestext']='Select Your File';
+
+$string['filename']='File Name';
+$string['filewidth']='Width';
+$string['fileheight']='Height';
+$string['fileotherinfo']='Other';
+$string['alttext']='Alt Text';
+$string['px']='px';
+$string['noimage']='There is no image has been uploaded! Please click browse to upload new image. Image size must be 540x290 pixel.';
+$string['imagetoshow']='There is few images has been uploaded!';
+$string['imageorder']='Display Order';
+
+//for index 2
 $string['section1']='Section 1';
 $string['section2']='Section 2';
 $string['section3']='Section 3';
 $string['section4']='Section 4';
 $string['section5']='Section 2'; //section 5 is replacing section 2 for unknown reason
-$string['fieldname']='Section';
-
-
+$string['sections']='Sections';
+$string['save']='Save';
